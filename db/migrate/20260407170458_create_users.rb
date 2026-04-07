6824ad0a95cfe92d37a042bad3ec465809d4ab11
Abdur Rahman <khalil.ahmad1547@gmail.com>
2026-04-07 22:31:13 +0500

    chore: migration cleanup

--- db/migrate/20260407170458_create_users.rb
+++ db/migrate/20260407170458_create_users.rb
@@ -1,11 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[8.1]
   def change
     create_table :users do |t|
-      t.string :email_address, null: false
+      t.string :email, null: false
+      t.string :name, null: false
       t.string :password_digest, null: false
+
+      # OAuth fields
+      t.string :provider
+      t.string :uid
+      t.string :avatar_url
 
       t.timestamps
     end
-    add_index :users, :email_address, unique: true
+
+    add_index :users, :email, unique: true
+    add_index :users, [ :provider, :uid ], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_03_111155) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_07_170505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -46,23 +46,30 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_03_111155) do
     t.index ["user_id"], name: "index_refresh_tokens_on_user_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "avatar_url"
     t.datetime "created_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "name", default: "", null: false
+    t.string "email", null: false
+    t.string "name", null: false
+    t.string "password_digest", null: false
     t.string "provider"
-    t.datetime "remember_created_at"
-    t.datetime "reset_password_sent_at"
-    t.string "reset_password_token"
     t.string "uid"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
   add_foreign_key "blacklisted_tokens", "users"
   add_foreign_key "password_reset_tokens", "users"
   add_foreign_key "refresh_tokens", "users"
+  add_foreign_key "sessions", "users"
 end
